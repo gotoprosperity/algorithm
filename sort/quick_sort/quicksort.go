10beb9a9cb9f5ce516f33b1c6f9f4171ccda2b5d
Shadas <shadas.zhao@gmail.com>
2021-09-26 15:51:17 +0800

    Add quick sort in another way

--- sort/quick_sort/quicksort.go
+++ sort/quick_sort/quicksort.go
@@ -1,11 +1,13 @@
 package quick_sort
 
+import "fmt"
+
 /*
 	快速排序写法
 	这里以go实现方式为例
 */
 
-// 写法一：携带左右边界递归
+// 写法一：递归；在目标值两侧，依次找需要移动的，类似拉锁，最后合并到目标值上。
 func QuickSort1(arr []int) {
 	if len(arr) <= 1 {
 		return
@@ -42,4 +44,30 @@ func quickSort1(arr []int, left, right int) {
 	if right-pos > 1 {
 		quickSort1(arr, pos+1, right)
 	}
+}
+
+// 写法二：递归；从左边开始，遍历范围内所有值，丢到目标值左边或者右边。
+func QuickSort2(arr []int) {
+	if len(arr) <= 1 {
+		return
+	}
+	mid, pos := arr[0], 1
+	head, tail := 0, len(arr)-1
+	fmt.Println("begin", arr, head, tail, mid, pos)
+	for head < tail {
+		if arr[pos] > mid {
+			arr[pos], arr[tail] = arr[tail], arr[pos] // 大于标记位的，都丢到最后
+			tail--
+		} else {
+			arr[pos], arr[head] = arr[head], arr[pos] // 小于等于标记位的，都丢到最前面
+			head++
+			pos++ // 不能丢，随着往前放，pos的值要向后移动判断
+		}
+		fmt.Println("pos", pos, arr)
+	}
+	arr[head] = mid // 一轮循环完，定位元素位置确认
+	fmt.Println("end", arr, head, tail, mid, pos)
+	// 两边接着来
+	QuickSort2(arr[:head])
+	QuickSort2(arr[head+1:])
 }
--- sort/quick_sort/quicksort_test.go
+++ sort/quick_sort/quicksort_test.go
@@ -16,3 +16,14 @@ func TestQuickSort1(t *testing.T) {
 		t.Errorf("wrong arr with %v", arr)
 	}
 }
+
+func TestQuickSort2(t *testing.T) {
+	var (
+		arr []int
+	)
+	arr = []int{9, 7, 3, 2, 1, 5, 6, 4, 8}
+	QuickSort2(arr)
+	if !util.IsIntArrayEqual(arr, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+		t.Errorf("wrong arr with %v", arr)
+	}
+}
