f17129659bb2c1cb80335a43957025c28f6083c4
Shadas <shadas.zhao@gmail.com>
2021-09-27 15:04:42 +0800

    Add string array equal tool

--- util/array.go
+++ util/array.go
@@ -12,3 +12,16 @@ func IsIntArrayEqual(a1, a2 []int) bool {
 	}
 	return true
 }
+
+// IsStrArrayEqual compares two []string and returns if they are the same.
+func IsStrArrayEqual(a1, a2 []string) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i := 0; i < len(a1); i++ {
+		if a1[i] != a2[i] {
+			return false
+		}
+	}
+	return true
+}
