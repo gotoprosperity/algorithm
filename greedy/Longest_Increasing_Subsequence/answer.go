4dae170351d85fd824ab8749b27854b1b5f6fdbe
Shadas <shadas.zhao@gmail.com>
2021-10-11 18:31:02 +0800

    Add greedy problem with Longest Increasing Subsuquence

---
+++ greedy/Longest_Increasing_Subsequence/answer.go
@@ -0,0 +1,73 @@
+package Longest_Increasing_Subsequence
+
+func lengthOfLIS(nums []int) int {
+	//return lengthOfLISDP(nums)
+	return lengthOfLISGreedy(nums)
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// dp解法
+func lengthOfLISDP(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	var (
+		dp      = make([]int, len(nums))
+		max int = 1
+	)
+	for idx, _ := range dp {
+		dp[idx] = 1
+	}
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j <= i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = maxInt(dp[i], dp[j]+1)
+			}
+		}
+		max = maxInt(max, dp[i])
+	}
+	return max
+}
+
+// 贪心法构造具体数列解法
+func lengthOfLISGreedy(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	var (
+		lis = []int{nums[0]}
+	)
+	for i := 1; i < len(nums); i++ {
+		val := nums[i]
+		head, tail := lis[0], lis[len(lis)-1]
+		if val <= head {
+			lis[0] = val
+		} else if val > tail {
+			lis = append(lis, val)
+		} else {
+			lis[findPos(lis, val)] = val
+		}
+	}
+	//fmt.Println(lis) // 此处可以检验实际计算长度的子序列，不一定是合法的，但长度是正确的
+	return len(lis)
+}
+
+// 二分法找第一个>=val的位置
+func findPos(lis []int, val int) int {
+	l, r := 0, len(lis)-1
+	for l < r {
+		mid := l + (r-l)/2
+		if lis[mid] >= val {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
---
+++ greedy/Longest_Increasing_Subsequence/answer_test.go
@@ -0,0 +1,21 @@
+package Longest_Increasing_Subsequence
+
+import "testing"
+
+func TestLengthOfLIS(t *testing.T) {
+	if ret := lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 19, 101, 18}); ret != 5 {
+		t.Errorf("should be 4, wrong length with %d", ret)
+	}
+}
+
+func TestFindPos(t *testing.T) {
+	if pos := findPos([]int{1, 2, 3}, 1); pos != 0 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+	if pos := findPos([]int{1, 2, 3}, 2); pos != 1 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+	if pos := findPos([]int{1, 2, 4}, 3); pos != 2 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+}
