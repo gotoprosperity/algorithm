9c4ba28152abbceda685016a7eb23ade75185f97
Shadas <shadas.zhao@gmail.com>
2021-11-01 17:31:24 +0800

    Add decode ways with dp

---
+++ dynamic_programming/decode_ways/answer_test.go
@@ -0,0 +1,15 @@
+package decode_ways
+
+import "testing"
+
+func TestNumDecodings(t *testing.T) {
+	if ret := numDecodings("12"); ret != 2 {
+		t.Errorf("wrong answer with %d", ret)
+	}
+	if ret := numDecodings("226"); ret != 3 {
+		t.Errorf("wrong answer with %d", ret)
+	}
+	if ret := numDecodings("0"); ret != 0 {
+		t.Errorf("wrong answer with %d", ret)
+	}
+}
